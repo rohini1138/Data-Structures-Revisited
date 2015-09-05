81900e307e2ef18e47b8013d3df38b6c416e40b9
rahulraopune <rahulraopune@gmail.com>
2015-09-05 17:25:05 +0530

    QuickSort added

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+int part(int low,int high,int *a)
+{
+     int i,h=high,l=low,p,t;
+     p=a[(low+high)/2]; //assume pivot as middle element
+     while(low<high)
+     {
+                    while(a[l]<p)
+                    {
+                                   l++;
+                    }
+                    while(a[h]>p)
+                    {
+                                   h--;
+                    }
+                    if(l<h)
+                    {
+                                t=a[l];
+                                a[l]=a[h];
+                                a[h]=t;
+                    }
+                    else
+                    {
+                        t=p;
+                        p=a[l];
+                        a[l]=t;
+                        break;
+                    }
+     }
+     return h;
+}
+
+void quicksort(int l,int h,int *a)
+{
+  int index,i;
+  if(l<h)
+  {
+          index=part(l,h,a);
+          quicksort(l,index-1,a);
+          quicksort(index+1,h,a);
+  }
+}
+
+int main()
+{
+      int a[100],n,l,h,i;
+      cout<<"Enter number of elements:";
+      cin>>n;
+      cout<<"Enter the elements ";
+      for(i=0;i<n;i++)
+      cin>>a[i];
+      cout<<"\nInitial Array:\n";
+      for(i=0;i<n;i++)
+      {
+                      cout<<a[i]<<" ";
+      }
+      h=n-1;
+      l=0;
+      quicksort(l,h,a);
+      cout<<"\nAfter Sorting:\n";
+      for(i=0;i<n;i++)
+      {
+                cout<<a[i]<<" ";
+      }
+      
+      return 0;
+}
+
